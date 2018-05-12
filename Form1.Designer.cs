046d6796cb063445103772872bdd9b566f8b5ce8
readyvista <goneaway@163.com>
2018-05-13 00:49:13 +0800

    simple-player
    an simple music player develeped on C# with extra dlls,partly support showing the theme pics of songs selected.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ofbutton = new System.Windows.Forms.Button();
+            this.Playbutton = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BackGround = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ofbutton
+            // 
+            this.ofbutton.AutoSize = true;
+            this.ofbutton.Location = new System.Drawing.Point(46, 13);
+            this.ofbutton.Name = "ofbutton";
+            this.ofbutton.Size = new System.Drawing.Size(138, 48);
+            this.ofbutton.TabIndex = 0;
+            this.ofbutton.Text = "open";
+            this.ofbutton.UseVisualStyleBackColor = true;
+            this.ofbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Playbutton
+            // 
+            this.Playbutton.Location = new System.Drawing.Point(263, 12);
+            this.Playbutton.Name = "Playbutton";
+            this.Playbutton.Size = new System.Drawing.Size(135, 48);
+            this.Playbutton.TabIndex = 1;
+            this.Playbutton.Text = "play";
+            this.Playbutton.UseVisualStyleBackColor = true;
+            this.Playbutton.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(46, 97);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(13, 10);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(43, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(405, 74);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BackGround
+            // 
+            this.BackGround.Location = new System.Drawing.Point(450, 12);
+            this.BackGround.Name = "BackGround";
+            this.BackGround.Size = new System.Drawing.Size(166, 49);
+            this.BackGround.TabIndex = 6;
+            this.BackGround.Text = "Change Background";
+            this.BackGround.UseVisualStyleBackColor = true;
+            this.BackGround.Click += new System.EventHandler(this.BackGround_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(174, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 50);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 45);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // addbutton
+            // 
+            this.addbutton.Location = new System.Drawing.Point(450, 316);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(33, 37);
+            this.addbutton.TabIndex = 13;
+            this.addbutton.Text = "+";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(88, 252);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(317, 409);
+            this.listBox1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(450, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 39);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(454, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(131, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 44);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(648, 662);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BackGround);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.Playbutton);
+            this.Controls.Add(this.ofbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ofbutton;
+        private System.Windows.Forms.Button Playbutton;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BackGround;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using ID3;
+using ID3.ID3v2Frames.BinaryFrames;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Security.Permissions;
+
+
+namespace player
+{
+  
+
+    public partial class Form1 : Form
+    {
+        bool IsLeft;
+        Point MouseMove;
+
+        
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            ofbutton.AutoSize = false;
+            label3.Font = new Font("微软雅黑", 15, FontStyle.Bold);
+            label2.Font = new Font("微软雅黑", 15, FontStyle.Bold);
+            label2.Text = " ";
+            label3.Text = " ";
+            BackGround.Text = "Change Background";
+            timer1.Interval = 100;
+            button1.Text = "AutoRewind";
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                Playbutton.Text = "Play";
+            }
+            label3.BackColor = System.Drawing.Color.Transparent;
+            label2.BackColor = System.Drawing.Color.Transparent;
+
+            this.label1.Visible = false;
+            while (Playbutton.Text == "pause")
+            {
+                timer1.Enabled = false;
+            }
+            this.listBox1.MouseDoubleClick += new MouseEventHandler(listBox1_MouseDoubleClick);
+            this.button3.Text = "shuffle";
+
+        }
+        string filepath = null;
+        OpenFileDialog of1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            /*if(listBox1.SelectedItem.ToString() != null)
+            {
+                of1.FileName = listBox1.SelectedItem.ToString();
+                if (of1.FileName != null)
+                {
+                    ID3.ID3Info info = new ID3.ID3Info(of1.FileName, true);
+                    foreach (AttachedPictureFrame AP in info.ID3v2Info.AttachedPictureFrames.Items)
+                    {
+
+                        pictureBox1.Image = Image.FromStream(AP.Data); //添加到PicturBOX内
+
+                    }
+                    
+                }
+                goto aa;
+            aa:
+                int count = 0;
+                filepath = of1.FileName;
+                this.axWindowsMediaPlayer1.URL = of1.FileName;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                timer1.Start();
+                label2.Text = axWindowsMediaPlayer1.Ctlcontrols.currentItem.name;
+
+                listBox1.Items.Add(of1.FileName);
+                count++;
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                Playbutton.Text = "pause";
+                return;
+            }*/
+            
+            of1 = new OpenFileDialog();
+            of1.InitialDirectory = "C://";
+            of1.Filter = "(音乐文件)*mp3|*mp3";
+            
+            
+            if (of1.ShowDialog() == DialogResult.OK)
+            {
+               if (of1.FileName!=null)
+                {
+                    ID3.ID3Info info = new ID3.ID3Info(of1.FileName, true);
+                    foreach (AttachedPictureFrame AP in info.ID3v2Info.AttachedPictureFrames.Items)
+                    {
+                        
+                        pictureBox1.Image = Image.FromStream(AP.Data); //添加到PicturBOX内
+
+                    }
+                    goto aa;
+                }
+           aa: 
+                int count = 0;
+                filepath = of1.FileName;
+                this.axWindowsMediaPlayer1.URL = of1.FileName;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                timer1.Start();
+                label2.Text = axWindowsMediaPlayer1.Ctlcontrols.currentItem.name;
+                
+                listBox1.Items.Add(of1.FileName);
+                count++;
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;          
+                /*new Thread(() =>
+                {
+                    while (true)
+                    {
+                        try { label3.BeginInvoke(new MethodInvoker(() => label3.Text = axWindowsMediaPlayer1.Ctlcontrols.currentPositionString)); }
+                        catch { }
+                        Thread.Sleep(1000);
+                    }
+                }) { IsBackground = true }.Start();*/
+                Playbutton.Text = "pause";
+                
+               
+            }
+            
+            
+           if (1 == (int)axWindowsMediaPlayer1.playState)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+            
+            
+            
+            if (axWindowsMediaPlayer1.Ctlcontrols.get_isAvailable("pause"))
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                Playbutton.Text = "play"; 
+            }
+
+            else if (axWindowsMediaPlayer1.Ctlcontrols.get_isAvailable("play"))
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                Playbutton.Text = "pause";
+            }
+   
+        }
+
+        private void BackGround_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of2 = new OpenFileDialog();
+            of2.InitialDirectory = "C://";
+            of2.Filter = "图像文件|*.jpg";
+            if (of2.ShowDialog() == DialogResult.OK)
+            {
+                this.BackgroundImage = Image.FromFile(of2.FileName);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            bool judge;
+            judge=axWindowsMediaPlayer1.settings.getMode("loop");
+            if (!judge)
+            {
+                axWindowsMediaPlayer1.settings.setMode("loop",true);
+                button1.Text="AutoRewind:ON";
+                button3.Text = "shuffle:OFF";
+            }
+            else
+            {
+                axWindowsMediaPlayer1.settings.setMode("loop", false);
+                button1.Text = "AutoRewind:OFF";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            timer1.Interval=1000;
+            label3.Text = axWindowsMediaPlayer1.Ctlcontrols.currentPositionString;
+            while (label3.Text == axWindowsMediaPlayer1.Ctlcontrols.currentItem.durationString)
+            {
+                timer1.Stop();
+            }
+        }
+
+        int axWindowsMediaPlayer1_PlayStateChange()
+        {
+            int check;
+            check = (int)axWindowsMediaPlayer1.playState;
+            return check;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            if (listBox1.SelectedItem.ToString() != null)
+            {
+                this.axWindowsMediaPlayer1.URL = listBox1.SelectedItem.ToString();
+                label2.Text = axWindowsMediaPlayer1.Ctlcontrols.currentItem.name;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                timer1.Start();
+            }
+        }      
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of1;
+
+            of1 = new OpenFileDialog();
+            of1.InitialDirectory = "F://";
+            of1.Filter = "(音乐文件)*mp3|*mp3";
+
+            if (of1.ShowDialog() == DialogResult.OK)
+            {
+
+                if (listBox1.Items.Contains(of1.FileName))
+                {
+                    MessageBox.Show("it's been selected!");
+                    return;
+                }
+                listBox1.Items.Add(of1.FileName);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*if (this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name != null)
+            {
+                this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name = listBox1.SelectedItem.ToString();
+                this.axWindowsMediaPlayer1.URL = listBox1.SelectedItem.ToString();
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }*/
+            /*axWindowsMediaPlayer1.Ctlcontrols.currentItem.name = listBox1.SelectedItem.ToString();
+            this.axWindowsMediaPlayer1.URL = listBox1.SelectedItem.ToString();
+            label2.Text = axWindowsMediaPlayer1.Ctlcontrols.currentItem.name;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            /*if (this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name == null)
+            {
+                this.axWindowsMediaPlayer1.Ctlcontrols.currentItem.name = listBox1.SelectedItem.ToString();
+                this.axWindowsMediaPlayer1.URL = listBox1.SelectedItem.ToString();
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }*/
+            if (listBox1.SelectedItem == null) return;
+            this.axWindowsMediaPlayer1.URL = listBox1.SelectedItem.ToString();
+            label2.Text = axWindowsMediaPlayer1.Ctlcontrols.currentItem.name;
+            timer1.Start();
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem!=null)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem); 
+            }
+            else
+            {
+                MessageBox.Show("please select an item!");
+                return;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            bool judging;
+            judging = axWindowsMediaPlayer1.settings.getMode("shuffle");
+            if (!judging)
+            {
+                axWindowsMediaPlayer1.settings.setMode("shuffle", true);
+                button3.Text = "shuffle:ON";
+                button1.Text = "AutoRewind:OFF";
+            }
+            else
+            {
+                axWindowsMediaPlayer1.settings.setMode("loop", false);
+                button3.Text = "shuffle:OFF";
+                button1.Text = "AutoRewind";
+            }
+            
+        }       
+    }
+    
+}
+
+
